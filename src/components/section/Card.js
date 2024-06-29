b75c1966cd86a3fe0d3f11dbfc55bd4ce3e76fed
Eder-TS <steder20@yahoo.com.br>
2024-06-29 08:19:45 -0300

    Added effect to presentation title and to skills

--- src/components/section/Card.js
+++ src/components/section/Card.js
@@ -1,22 +1,36 @@
 import ButtonB from '../elements/ButtonB'
 import styles from './Card.module.css'
+import { useState } from 'react'
 
 function Card({site, img, title, tech, description, repo}){
+    const [info, setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+
+    function InfoOff(){
+        setInfo(false)
+    }
+
     return(
-        <div className={styles.card}>
-            <a href={site}>
+        <div className={styles.card} onMouseLeave={InfoOff}>
+            <a href={site} onMouseEnter={InfoOn}>
                 <img src={img} alt=''/>
             </a>
 
-            <section>
-                <h3>{title}</h3>
+            {info === true &&(
+                <section>
+                    <h3>{title}</h3>
 
-                <p><strong>Tecnologias:</strong> {tech}</p>
+                    <p><strong>Tecnologias:</strong> {tech}</p>
 
-                <p>{description}</p>
+                    <p>{description}</p>
 
-                <ButtonB text='Acesse o repositório' link={repo}/>
-            </section> 
+                    <ButtonB text='Acesse o repositório' link={repo}/>
+                </section>
+            )}
+
         </div>
     )
 }
--- src/components/section/Presentation.js
+++ src/components/section/Presentation.js
@@ -1,12 +1,44 @@
 import ButtonA from '../elements/ButtonA'
 import styles from './Presentation.module.css'
+import { useEffect, useState } from 'react'
 
 function Presentation(){
+    const [text, setText] = useState('')
+    const toRotate = ['Eder TS!', 'Desenvolvedor Full Stack!']
+    const [loop, setLoop] = useState(0)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const period = 180
+    const [delta, setDelta] = useState(100)
+
+    useEffect(() => {
+        let clock = setInterval(() => {
+            toType()
+        }, delta)
+        return() => {clearInterval(clock)}
+    }, [text])
+
+    const toType = () => {
+        let i = loop % toRotate.length
+        let fullText =  toRotate[i]
+        let updateText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+
+        setText(updateText)
+
+        if(!isDeleting && updateText === fullText){
+            setIsDeleting(true)
+            setDelta(period)
+        } else if (isDeleting && updateText === ''){
+            setIsDeleting(false)
+            setDelta(period)
+            setLoop(loop + 1)
+        }
+    }
+
     return(
         <div className={styles.presentation} id="presentation">
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
 
-            <h1><strong>Olá, eu sou Eder TS</strong></h1>
+            <h1><strong>Olá, eu sou {text}</strong></h1>
 
             <p>
                 Sou um apaixonado por tecnologia e soluções inovadoras. Como<br/>
